079a0fb4ca3e05eb1abae54a479b03d33b60c039
umfrancisco <francis.guitler@gmail.com>
2025-09-04 09:27:48 -0300

    modified task list

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,18 +1,40 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar javascript',
+    descricao: 'Ver aula 1',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Estudar python',
+    descricao: 'Ver aula 1',
+    prioridade: 'urgente',
+    status: 'concluído'
+  },
+  {
+    titulo: 'Estudar react',
+    descricao: 'Ver aula 1',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            titulo={t.titulo}
+            descricao={t.descricao}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
